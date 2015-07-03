80289f4f5ce6f017a0463447b16cd31ac58ce848
yuhc <yuhc123@gmail.com>
2015-07-03 12:22:25 +0800

    Fix several errors

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+echo "Script executed from: ${PWD}"
+SCPATH=${PWD}
 YUPATH="$HOME/.yuhc-vim"
 
 echo "# Backing up current vim config"
@@ -15,9 +17,9 @@ cp -vi .gvimrc $YUPATH
 cp -vi *.vim $YUPATH
 cp -avi colors $YUPATH
 
-ln -vs $YUPATH $HOME/.vim
-ln -vs $YUPATH/.vimrc $HOME/.vimrc
-ln -vs $YUPATH/.gvimrc $HOME/.gvimrc
+ln -vsf $YUPATH $HOME/.vim
+ln -vsf $YUPATH/.vimrc $HOME/.vimrc
+ln -vsf $YUPATH/.gvimrc $HOME/.gvimrc
 
 echo "# Installing Vundle"
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
@@ -29,19 +31,17 @@ echo "# Installing Vim plugins customizations"
 echo "# - YouCompleteMe"
 $YUPATH/bundle/YouCompleteMe/install.sh --clang-completer --system-libclang
 YCMPATH="$YUPATH/bundle/YouCompleteMe"
-test -d "$YCMPATH" || mkdir -p "$YCMPATH" && cp -vi bundle/YouCompleteMe/* "$YCMPATH"
+test -d "$YCMPATH" || mkdir -p "$YCMPATH" && cp -vi $SCPATH/bundle/YouCompleteMe/* "$YCMPATH"
 
 echo "# - MatchTag custom filetypes"
 FTPATH="$YUPATH/bundle/MatchTag/ftplugin"
-test -d "$FTPATH" || mkdir -p "$FTPATH" && cp -vi bundle/MatchTag/ftplugin/* "$FTPATH"
+test -d "$FTPATH" || mkdir -p "$FTPATH" && cp -vi $SCPATH/bundle/MatchTag/ftplugin/* "$FTPATH"
 
 echo "# - the_silver_searcher"
 $YUPATH/bundle/the_silver_searcher/build.sh
 cd $YUPATH/bundle/the_silver_searcher
 make install
-cd $YUPATH
 
 echo "# - c-support (c.vim)"
 CSPATH="$YUPATH/bundle/c-support"
-test -d "$CSPATH" || mkdir -p "$CSPATH" && cp -vi bundle/c-support/*
-"$CSPATH"
+test -d "$CSPATH" || mkdir -p "$CSPATH" && cp -vi $SCPATH/bundle/c-support/* "$CSPATH"
